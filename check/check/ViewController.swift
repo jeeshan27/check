968efc13d99acb3dd990bbb2e907726f9a4c2a39
Jeeshan Ali <jeeshan@Jeeshans-Mac.local>
2017-09-01 18:59:47 +0530

    initial commit

---
+++ check/check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  check
+//
+//  Created by Jeeshan Ali on 01/09/17.
+//  Copyright © 2017 Jeeshan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
